2b2c8851f1dee10f8bb4266b299df5fd8ee5f238
doheon <heizence6626@gmail.com>
2026-03-07 20:41:09 +0900

    feat : add vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./public",
+  build: {
+    outDir: "../dist/public",
+    emptyOutDir: true,
+  },
+  server: {
+    port: 8080,
+  },
+});
